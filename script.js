5bde642c1ad7cca683d04eaba1c801928395c7d5
Marvellous <adeoyemarvellous7@gmail.com>
2022-09-16 13:51:10 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let smallLots = document.getElementsByClassName('small-lot-btn')
+let multiLots = document.getElementsByClassName('multi-lot-btn')
+
+for (let smallLot of smallLots) {
+    smallLot.addEventListener('click', lotSelected)
+
+    function lotSelected() {
+        smallLot.parentElement.style.backgroundColor = "red"
+        smallLot.innerHTML = '<h1>Booked</h1>'
+        setTimeout(
+            () => {
+                smallLot.parentElement.style.backgroundColor = "purple";
+                smallLot.textContent = "I'm free, book me";
+                smallLot.previousElementSibling.style.display = 'block';
+            }, 2000)
+    }
+}
+
+for (let multiLot of multiLots) {
+    multiLot.addEventListener('click', lotSelected)
+
+    function lotSelected() {
+        multiLot.parentElement.style.backgroundColor = "red"
+
+        multiLot.innerHTML = '<h1>Booked</h1>'
+        setTimeout(
+            () => {
+                multiLot.parentElement.style.backgroundColor = "purple";
+                multiLot.textContent = "I'm free, book me";
+                multiLot.previousElementSibling.classList.remove('renew-btn');
+            }, 5400000)
+    }
+}
+
